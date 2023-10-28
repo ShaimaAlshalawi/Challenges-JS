3805bd4b1e3a6421b61fd5efbadaedbf5c6bcb33
Shaima Alshalawi <shaima.alshalawi@gmail.com>
2023-10-28 23:48:58 +0300

    Create less_or_more_than_zero.js

---
+++ easy/less_or_more_than_zero.js
@@ -0,0 +1,9 @@
+// موقعي في خط الأعداد الصحيحة
+function less_or_more_than_zero(number) {
+    if (number < 0) 
+     return "Less than zero";
+    else if (number > 0)
+        return "Greater than zero"; 
+    else 
+    return "Equal to zero"; 
+}
