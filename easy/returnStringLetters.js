6c95cd2549516f9d583ae46cb23f9894906dc170
Shaima Alshalawi <shaima.alshalawi@gmail.com>
2023-10-28 23:50:25 +0300

    Create returnStringLetters.js

---
+++ easy/returnStringLetters.js
@@ -0,0 +1,10 @@
+// مقارنة أطول عدد خانات لنصين
+function returnStringLetters(string1, string2) {
+    var len1=string1.length;
+    var len2=string2.length;
+    if (len1<len2)
+        return len2;
+    else
+        return len1
+
+}
